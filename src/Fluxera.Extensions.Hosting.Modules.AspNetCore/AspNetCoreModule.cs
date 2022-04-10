be7862ccbeb659ac4877694221a11fcdb000641c
Matthias Gernand <mgernand@users.noreply.github.com>
2022-04-11 00:57:27 +0200

    Added httpcontext-based principal provider module and a base ASP.NET Core module tat enables basic services. (#4)

---
+++ src/Fluxera.Extensions.Hosting.Modules.AspNetCore/AspNetCoreModule.cs
@@ -0,0 +1,26 @@
+﻿namespace Fluxera.Extensions.Hosting.Modules.AspNetCore
+{
+	using Fluxera.Extensions.Hosting.Modules.Configuration;
+	using JetBrains.Annotations;
+	using Microsoft.Extensions.DependencyInjection;
+
+	/// <summary>
+	///     A module that enables ASP.NET Core basic features.
+	/// </summary>
+	[PublicAPI]
+	[DependsOn(typeof(ConfigurationModule))]
+	public sealed class AspNetCoreModule : ConfigureServicesModule
+	{
+		/// <inheritdoc />
+		public override void ConfigureServices(IServiceConfigurationContext context)
+		{
+			// Add the http context accessor.
+			context.Log("AddHttpContextAccessor",
+				services => services.AddHttpContextAccessor());
+
+			// Add the action context accessor.
+			context.Log("AddActionContextAccessor",
+				services => services.AddActionContextAccessor());
+		}
+	}
+}
---
+++ tests/Fluxera.Extensions.Hosting.Modules.AspNetCore.HttpPrincipal.UnitTests/TestModule.cs
@@ -0,0 +1,15 @@
+﻿namespace Fluxera.Extensions.Hosting.Modules.AspNetCore.HttpPrincipal.UnitTests
+{
+	using Fluxera.Extensions.DependencyInjection;
+	using Microsoft.AspNetCore.Http;
+
+	[DependsOn(typeof(HttpPrincipalModule))]
+	public class TestModule : ConfigureServicesModule
+	{
+		/// <inheritdoc />
+		public override void ConfigureServices(IServiceConfigurationContext context)
+		{
+			context.Services.ReplaceSingleton<IHttpContextAccessor, TestHttpContextAccessor>();
+		}
+	}
+}

8f06492442abee243fadbe9527ed223d8e68ac9a
Matthias Gernand <matthias.gernand@fluxera.com>
2022-04-19 22:06:14 +0200

    Added EF Core module.

---
+++ src/Fluxera.Extensions.Hosting.Modules.Persistence.EntityFrameworkCore/EntityFrameworkCorePersistenceModule.cs
@@ -0,0 +1,21 @@
+﻿namespace Fluxera.Extensions.Hosting.Modules.Persistence.EntityFrameworkCore
+{
+	using Fluxera.Extensions.Hosting.Modules.Persistence.EntityFrameworkCore.Contributors;
+	using JetBrains.Annotations;
+
+	/// <summary>
+	///     A module that enables EntityFramework Core persistence.
+	/// </summary>
+	[PublicAPI]
+	[DependsOn(typeof(PersistenceModule))]
+	public sealed class EntityFrameworkCorePersistenceModule : ConfigureServicesModule
+	{
+		/// <inheritdoc />
+		public override void PreConfigureServices(IServiceConfigurationContext context)
+		{
+			// Add the repository provider contributor.
+			context.Log("AddRepositoryProviderContributor(EntityFrameworkCore)",
+				services => services.AddRepositoryProviderContributor<RepositoryProviderContributor>());
+		}
+	}
+}

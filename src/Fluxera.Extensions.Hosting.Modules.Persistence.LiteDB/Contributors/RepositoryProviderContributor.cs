a173cf0fdcbfb9db57b1507dd478bec63381b617
Matthias Gernand <matthias.gernand@fluxera.com>
2022-04-19 22:07:01 +0200

    Added LiteDB persistence module.

---
+++ src/Fluxera.Extensions.Hosting.Modules.Persistence.LiteDB/Contributors/RepositoryProviderContributor.cs
@@ -0,0 +1,38 @@
+﻿namespace Fluxera.Extensions.Hosting.Modules.Persistence.LiteDB.Contributors
+{
+	using System;
+	using Fluxera.Repository;
+	using Fluxera.Repository.LiteDB;
+	using JetBrains.Annotations;
+
+	[UsedImplicitly]
+	internal sealed class RepositoryProviderContributor : IRepositoryProviderContributor
+	{
+		public string RepositoryProviderName => RepositoryProviderNames.InMemory;
+
+		public Action<IRepositoryBuilder, string, Action<IRepositoryOptionsBuilder>> AddRepository
+		{
+			get
+			{
+				return (builder, repositoryName, optionsAction) =>
+				{
+					builder.AddLiteRepository(repositoryName, optionsAction, mapper =>
+					{
+						// TODO: How do we get this special configuration in here?
+					});
+				};
+			}
+		}
+
+		public Action<IRepositoryOptionsBuilder, string, RepositoryOptions> ConfigureRepository
+		{
+			get
+			{
+				return (builder, connectionString, options) =>
+				{
+					builder.AddSetting("Lite.Database", connectionString);
+				};
+			}
+		}
+	}
+}

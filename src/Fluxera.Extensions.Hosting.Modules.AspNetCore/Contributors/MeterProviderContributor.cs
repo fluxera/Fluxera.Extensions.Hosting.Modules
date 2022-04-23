c9a5f642da835e96d2fbc486dd88c8f4be425cc5
Matthias Gernand <matthias.gernand@fluxera.com>
2022-04-23 21:44:40 +0200

    Added ASP.NET Core intrumentation for OpenTelemetry.

---
+++ src/Fluxera.Extensions.Hosting.Modules.AspNetCore/Contributors/MeterProviderContributor.cs
@@ -0,0 +1,14 @@
+﻿namespace Fluxera.Extensions.Hosting.Modules.AspNetCore.Contributors
+{
+	using Fluxera.Extensions.Hosting.Modules.OpenTelemetry;
+	using global::OpenTelemetry.Metrics;
+
+	internal sealed class MeterProviderContributor : IMeterProviderContributor
+	{
+		/// <inheritdoc />
+		public void Configure(MeterProviderBuilder builder)
+		{
+			builder.AddAspNetCoreInstrumentation();
+		}
+	}
+}

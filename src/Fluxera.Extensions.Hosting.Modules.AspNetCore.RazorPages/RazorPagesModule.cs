3264425cdde61d5db51e53d12a5b1cee120195de
Matthias Gernand <matthias.gernand@fluxera.com>
2022-05-07 23:29:09 +0200

    Added razor pages module.

---
+++ src/Fluxera.Extensions.Hosting.Modules.AspNetCore.RazorPages/RazorPagesModule.cs
@@ -0,0 +1,27 @@
+﻿namespace Fluxera.Extensions.Hosting.Modules.AspNetCore.RazorPages
+{
+	using Fluxera.Extensions.Hosting.Modules.AspNetCore.RazorPages.Contributors;
+	using JetBrains.Annotations;
+	using Microsoft.Extensions.DependencyInjection;
+
+	/// <summary>
+	///     A module that enables Razor Pages for ASP.NET Core.
+	/// </summary>
+	[PublicAPI]
+	[DependsOn(typeof(AspNetCoreModule))]
+	public sealed class RazorPagesModule : ConfigureServicesModule
+	{
+		/// <inheritdoc />
+		public override void PreConfigureServices(IServiceConfigurationContext context)
+		{
+			// Add the route endpoint contributor.
+			context.Services.AddEndpointRouteContributor<EndpointRouteContributor>();
+		}
+
+		/// <inheritdoc />
+		public override void ConfigureServices(IServiceConfigurationContext context)
+		{
+			context.Log("AddRazorPages", services => services.AddRazorPages());
+		}
+	}
+}

086ec6d7441661704ce02750ef5aa6ae5133171a
Matthias Gernand <mgernand@users.noreply.github.com>
2022-05-07 23:29:33 +0200

    Added razor pages module. (#21)

---
+++ src/Fluxera.Extensions.Hosting.Modules.AspNetCore.RazorPages/Contributors/EndpointRouteContributor.cs
@@ -0,0 +1,22 @@
+﻿namespace Fluxera.Extensions.Hosting.Modules.AspNetCore.RazorPages.Contributors
+{
+	using JetBrains.Annotations;
+	using Microsoft.AspNetCore.Builder;
+	using Microsoft.AspNetCore.Routing;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.Logging;
+
+	[UsedImplicitly]
+	internal sealed class EndpointRouteContributor : IEndpointRouteContributor
+	{
+		public void MapRoute(IEndpointRouteBuilder routeBuilder)
+		{
+			ILogger<EndpointRouteContributor> logger = routeBuilder.ServiceProvider.GetRequiredService<ILogger<EndpointRouteContributor>>();
+
+			logger.LogDebug("UseEndpoints -> MapControllers()");
+			routeBuilder.MapRazorPages();
+		}
+
+		public int Position => 1000;
+	}
+}

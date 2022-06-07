4a22b9f3b1992be92de72d25d947ff526dc9ba07
Matthias Gernand <mgernand@users.noreply.github.com>
2022-06-08 00:29:26 +0200

    Updated packages. (#33)
    * Updated packages.
    
    * Updated packages.
    
    * Updated packages.
    
    * Updated packages.

--- samples/Example.Application/Services/ExampleApplicationService.cs
+++ samples/Example.Application/Services/ExampleApplicationService.cs
@@ -7,13 +7,14 @@
 	using Example.Domain.ExampleAggregate.Model;
 	using Example.Domain.ExampleAggregate.Repositories;
 	using Example.Domain.Shared.ExampleAggregate.Model;
+	using Fluxera.Repository;
 	using JetBrains.Annotations;
 
 	[UsedImplicitly]
 	internal sealed class ExampleApplicationService : IExampleApplicationService
 	{
 		private readonly IMapper mapper;
-		private readonly IExampleRepository repository;
+		private readonly IRepository<Example, ExampleId> repository;
 
 		public ExampleApplicationService(IExampleRepository repository, IMapper mapper)
 		{
@@ -24,7 +25,7 @@
 		/// <inheritdoc />
 		public async Task<ExampleDto> GetExampleAsync(ExampleId id)
 		{
-			Example entity = await this.repository.GetAsync(id);
+			Example entity = await this.repository.FindOneAsync(x => x.ID == id);
 			ExampleDto dto = this.mapper.Map<ExampleDto>(entity);
 
 			return dto;

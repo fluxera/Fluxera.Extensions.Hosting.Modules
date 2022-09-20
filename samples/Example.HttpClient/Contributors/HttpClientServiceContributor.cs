2d5dfaa6c1f9593f9a2bb8c2f40e7f0f6c2dbc4a
Matthias Gernand <matthias.gernand@fluxera.com>
2022-09-20 21:08:13 +0200

    Updated packages.

---
+++ samples/Example.HttpClient/Contributors/HttpClientServiceContributor.cs
@@ -0,0 +1,24 @@
+﻿namespace Example.HttpClient.Contributors
+{
+	using Example.Application.Contracts.Services;
+	using Example.HttpClient.Services;
+	using Fluxera.Extensions.Hosting;
+	using Fluxera.Extensions.Hosting.Modules.HttpClient;
+	using Microsoft.Extensions.DependencyInjection;
+
+	internal sealed class HttpClientServiceContributor : IHttpClientServiceContributor
+	{
+		/// <inheritdoc />
+		public IHttpClientBuilder AddNamedHttpClientService(IServiceConfigurationContext context)
+		{
+			IHttpClientBuilder httpClientBuilder = context.Services.AddHttpClientService<IExampleApplicationService, ExampleApplicationServiceClient>(
+				ctx =>
+				{
+					ExampleApplicationServiceClient client = new ExampleApplicationServiceClient(ctx.Name, ctx.HttpClient, ctx.Options);
+					return client;
+				});
+
+			return httpClientBuilder;
+		}
+	}
+}
---
+++ samples/Example.HttpClient/Services/ExampleApplicationServiceClient.cs
@@ -0,0 +1,32 @@
+﻿namespace Example.HttpClient.Services
+{
+	using System.Net.Http;
+	using System.Threading.Tasks;
+	using Example.Application.Contracts.Dtos;
+	using Example.Application.Contracts.Services;
+	using Example.Domain.Shared.ExampleAggregate.Model;
+	using Fluxera.Extensions.Http;
+	using JetBrains.Annotations;
+
+	[UsedImplicitly]
+	internal sealed class ExampleApplicationServiceClient : HttpClientServiceBase, IExampleApplicationService, IHttpClientService
+	{
+		/// <inheritdoc />
+		public ExampleApplicationServiceClient(string name, HttpClient httpClient, RemoteService options)
+			: base(name, httpClient, options)
+		{
+		}
+
+		/// <inheritdoc />
+		public async Task<ExampleDto> GetExampleAsync(ExampleId id)
+		{
+			return null;
+		}
+
+		/// <inheritdoc />
+		public async Task<ExampleDto> AddExample(ExampleDto item)
+		{
+			return null;
+		}
+	}
+}

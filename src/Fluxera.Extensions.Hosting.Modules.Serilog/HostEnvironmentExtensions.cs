e8811f47986c4fab666071518fbd29b617148ec3
Matthias Gernand <mgernand@users.noreply.github.com>
2022-10-10 15:06:15 +0200

    Added extension methods for getting the default log file path for Serilog. (#44)

---
+++ src/Fluxera.Extensions.Hosting.Modules.Serilog/HostEnvironmentExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Fluxera.Extensions.Hosting.Modules.Serilog
+{
+	using System;
+	using System.IO;
+	using JetBrains.Annotations;
+	using Microsoft.Extensions.Hosting;
+
+	/// <summary>
+	///     Extensions methods for the <see cref="IHostEnvironment" /> type.
+	/// </summary>
+	[PublicAPI]
+	public static class HostEnvironmentExtensions
+	{
+		/// <summary>
+		///     Gets the default log file path for Serilog.
+		/// </summary>
+		/// <param name="environment"></param>
+		/// <returns></returns>
+		public static string GetDefaultLogFilePath(this IHostEnvironment environment)
+		{
+			string baseDirectory = AppContext.BaseDirectory;
+			string logFilePath = Path.Combine(baseDirectory, "logs", environment.ApplicationName + "_.log");
+			return logFilePath;
+		}
+	}
+}

21a863c03d7ec74294dd75152f3c12164c5c8449
Matthias Gernand <matthias.gernand@fluxera.com>
2022-12-09 12:04:29 +0100

    Updated packages.

--- samples/Example.Infrastructure/ExampleDbContext.cs
+++ samples/Example.Infrastructure/ExampleDbContext.cs
@@ -5,6 +5,7 @@
 	using Fluxera.Repository.EntityFrameworkCore;
 	using MassTransit;
 	using Microsoft.EntityFrameworkCore;
+	using Microsoft.EntityFrameworkCore.Diagnostics;
 	using Microsoft.Extensions.Logging;
 
 	public sealed class ExampleDbContext : DbContext
@@ -28,33 +29,46 @@
 		/// <inheritdoc />
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
+			// System.InvalidOperationException: An error was generated for warning 'Microsoft.EntityFrameworkCore.Database.Transaction.TransactionIgnoredWarning':
+			// Transactions are not supported by the in-memory store. See http://go.microsoft.com/fwlink/?LinkId=800142 This exception can be suppressed or logged
+			// by passing event ID 'InMemoryEventId.TransactionIgnoredWarning' to the 'ConfigureWarnings' method in 'DbContext.OnConfiguring' or 'AddDbContext'.
+
 			if(!optionsBuilder.IsConfigured)
 			{
 				RepositoryName repositoryName = new RepositoryName("Default");
 
 				string databaseName = this.databaseNameProvider?.GetDatabaseName(repositoryName);
-				string connectionString = this.databaseConnectionStringProvider?.GetConnectionString(repositoryName);
+				string connectionStringEx = this.databaseConnectionStringProvider?.GetConnectionString(repositoryName);
 
 				this.logger.LogInformation("Using database name: '{DatabaseName}'.", databaseName);
-				this.logger.LogInformation("Using connection string: '{ConnectionString}'.", connectionString);
+				this.logger.LogInformation("Using connection string: '{ConnectionString}'.", connectionStringEx);
 
-				optionsBuilder.UseInMemoryDatabase(databaseName ?? "default");
+				string connectionString =
+					$"Server=MARS;Database={databaseName};Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+				optionsBuilder.UseSqlServer(connectionString);
+
+				optionsBuilder.ConfigureWarnings(builder =>
+				{
+					builder.Log(InMemoryEventId.TransactionIgnoredWarning);
+				});
 			}
 		}
 
 		/// <inheritdoc />
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
-			modelBuilder.AddInboxStateEntity();
-			modelBuilder.AddOutboxMessageEntity();
-			modelBuilder.AddOutboxStateEntity();
-
 			modelBuilder.Entity<Domain.Example.Example>(entity =>
 			{
 				entity.ToTable("Examples");
+
+				entity.OwnsOne(x => x.Detail);
 			});
 
 			modelBuilder.UseRepositoryDefaults();
+
+			modelBuilder.AddInboxStateEntity();
+			modelBuilder.AddOutboxMessageEntity();
+			modelBuilder.AddOutboxStateEntity();
 		}
 	}
 }

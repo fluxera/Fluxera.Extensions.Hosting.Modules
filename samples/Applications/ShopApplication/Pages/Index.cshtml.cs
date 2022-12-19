d776a3067b96d000f105f7dcdee3604fcfa77164
Matthias Gernand <matthias.gernand@fluxera.com>
2022-12-19 17:59:01 +0100

    Added frontend.

---
+++ samples/Applications/ShopApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿namespace ShopApplication.Pages
+{
+	using Microsoft.AspNetCore.Mvc.RazorPages;
+
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> logger;
+
+		public IndexModel(ILogger<IndexModel> logger)
+		{
+			this.logger = logger;
+		}
+
+		public void OnGet()
+		{
+		}
+	}
+}

64588e34bfac363bda800e3cd192477fc3127ca8
Matthias Gernand <matthias.gernand@fluxera.com>
2022-12-22 08:49:16 +0100

    Renamed http client module classes.

--- samples/Components/Ordering/Ordering.HttpClient/OrderingHttpClientModule.cs
+++ samples/Components/Ordering/Ordering.HttpClient/OrderingHttpClientModule.cs
@@ -1,16 +1,16 @@
-﻿namespace Catalog.HttpClient
+﻿namespace Ordering.HttpClient
 {
-	using Catalog.HttpClient.Contributors;
 	using Fluxera.Extensions.Hosting;
 	using Fluxera.Extensions.Hosting.Modules;
 	using Fluxera.Extensions.Hosting.Modules.Configuration;
 	using Fluxera.Extensions.Hosting.Modules.HttpClient;
 	using JetBrains.Annotations;
+	using Ordering.HttpClient.Contributors;
 
 	[PublicAPI]
 	[DependsOn<HttpClientModule>]
 	[DependsOn<ConfigurationModule>]
-	public sealed class CatalogHttpApiModule : ConfigureApplicationModule
+	public sealed class OrderingHttpClientModule : ConfigureApplicationModule
 	{
 		/// <inheritdoc />
 		public override void PreConfigureServices(IServiceConfigurationContext context)

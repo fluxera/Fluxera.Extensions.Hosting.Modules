ee49c78279bc5dcb9074dcdba30b48b35b3adf64
Matthias Gernand <matthias.gernand@fluxera.com>
2023-01-06 20:35:18 +0100

    Fixed health checks endpoints configuration.

--- src/Fluxera.Extensions.Hosting.Modules.AspNetCore.HealthChecks/HealthChecksEndpointsModule.cs
+++ src/Fluxera.Extensions.Hosting.Modules.AspNetCore.HealthChecks/HealthChecksEndpointsModule.cs
@@ -1,6 +1,5 @@
 ﻿namespace Fluxera.Extensions.Hosting.Modules.AspNetCore.HealthChecks
 {
-	using Fluxera.Extensions.Hosting.Modules.AspNetCore;
 	using Fluxera.Extensions.Hosting.Modules.AspNetCore.HealthChecks.Contributors;
 	using Fluxera.Extensions.Hosting.Modules.Configuration;
 	using Fluxera.Extensions.Hosting.Modules.HealthChecks;
@@ -10,6 +9,7 @@
 	///     A module that enabled the health checks.
 	/// </summary>
 	[PublicAPI]
+	[DependsOn<AspNetCoreModule>]
 	[DependsOn<HealthChecksModule>]
 	[DependsOn<ConfigurationModule>]
 	public sealed class HealthChecksEndpointsModule : ConfigureServicesModule

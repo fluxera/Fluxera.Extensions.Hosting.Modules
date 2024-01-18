6cc3cb1bec0267b42e7c10e186b1df8082dee68c
Matthias Gernand <mgernand@users.noreply.github.com>
2024-01-18 16:39:40 +0100

    Extended the batch result DTO. (#126)

--- src/Fluxera.Extensions.Hosting.Modules.Application.Contracts/Dtos/BatchResultDto.cs
+++ src/Fluxera.Extensions.Hosting.Modules.Application.Contracts/Dtos/BatchResultDto.cs
@@ -10,10 +10,11 @@
 	/// </summary>
 	[PublicAPI]
 	[Serializable]
-	public sealed class BatchResultDto<TResult> where TResult : ResultBaseDto<TResult>
+	public sealed class BatchResultDto<TResult> : IResultDto
+		where TResult : class, IResultDto
 	{
 		/// <summary>
-		///		Initializes a new instance of the <see cref="BatchResultDto{TValue}"/> type.
+		///		Initializes a new instance of the <see cref="BatchResultDto{TResult}"/> type.
 		/// </summary>
 		public BatchResultDto()
 		{
@@ -28,19 +29,25 @@
 		}
 
 		/// <summary>
-		///		Flag, indicating if one of the batched result was successful.
-		/// </summary>
-		public bool IsSuccess => !this.IsFailed;
-
-		/// <summary>
 		///		Flag, indicating if one of the batched results was failed.
 		/// </summary>
 		public bool IsFailed => this.Results.Any(x => x.IsFailed);
 
 		/// <summary>
-		///		Gets or sets the results.
+		///		Flag, indicating if one of the batched result was successful.
 		/// </summary>
-		public IList<TResult> Results { get; set; }
+		public bool IsSuccessful => !this.IsFailed;
+
+		/// <inheritdoc />
+		public IList<ErrorDto> Errors => this.Results.SelectMany(x => x.Errors).ToList();
+
+		/// <inheritdoc />
+		public IList<SuccessDto> Successes => this.Results.SelectMany(x => x.Successes).ToList();
+
+		/// <summary>
+		///		Gets the results.
+		/// </summary>
+		public IList<TResult> Results { get; private set; }
 
 		/// <summary>
 		///		Creates a new batched result for the given results.
@@ -50,54 +57,6 @@
 		public BatchResultDto<TResult> Create(IEnumerable<TResult> results)
 		{
 			return new BatchResultDto<TResult>(results);
-		}
-	}
-
-	/// <summary>
-	///		A DTO for batched operation that may return multiple results with a value.
-	/// </summary>
-	[PublicAPI]
-	[Serializable]
-	public sealed class BatchResultDto<TResult, TValue> where TResult : ResultBaseDto<TResult, TValue>
-	{
-		/// <summary>
-		///		Initializes a new instance of the <see cref="BatchResultDto{TValue}"/> type.
-		/// </summary>
-		public BatchResultDto()
-		{
-			// Note: Needed for serialization.
-		}
-
-		private BatchResultDto(IEnumerable<TResult> results)
-		{
-			results ??= Enumerable.Empty<TResult>();
-
-			this.Results = new List<TResult>(results);
-		}
-
-		/// <summary>
-		///		Flag, indicating if one of the batched result was successful.
-		/// </summary>
-		public bool IsSuccess => !this.IsFailed;
-
-		/// <summary>
-		///		Flag, indicating if one of the batched results was failed.
-		/// </summary>
-		public bool IsFailed => this.Results.Any(x => x.IsFailed);
-
-		/// <summary>
-		///		Gets or sets the results.
-		/// </summary>
-		public IList<TResult> Results { get; set; }
-
-		/// <summary>
-		///		Creates a new batched result for the given results.
-		/// </summary>
-		/// <param name="results"></param>
-		/// <returns></returns>
-		public BatchResultDto<TResult, TValue> Create(IEnumerable<TResult> results)
-		{
-			return new BatchResultDto<TResult, TValue>(results);
 		}
 	}
 }

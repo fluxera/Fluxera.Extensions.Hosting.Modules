725625b0d3a2823db53c9490502c1d722e9926d6
Matthias Gernand <matthias.gernand@fluxera.com>
2024-01-25 17:13:03 +0100

    Added Automapper profile for mapping results to result dtos.

---
+++ src/Fluxera.Extensions.Hosting.Modules.Application/Contributors/ResultMappingProfile.cs
@@ -0,0 +1,28 @@
+﻿namespace Fluxera.Extensions.Hosting.Modules.Application.Contributors
+{
+	using System.Reflection;
+	using Fluxera.Extensions.Hosting.Modules.Application.Contracts.Dtos;
+	using global::AutoMapper;
+	using JetBrains.Annotations;
+	using MadEyeMatt.Results;
+
+	[UsedImplicitly]
+	internal sealed class ResultMappingProfile : Profile
+	{
+		public ResultMappingProfile()
+		{
+			this.CreateMap<IError, ErrorDto>();
+			this.CreateMap<ISuccess, SuccessDto>();
+			this.CreateMap(typeof(Result<>), typeof(ResultDto<>))
+				.ForMember("Value", options =>
+				{
+					options.PreCondition(src =>
+					{
+						PropertyInfo property = src.GetType().GetProperty(nameof(Result.IsSuccessful));
+						bool isSuccessful = (bool) (property?.GetValue(src) ?? false);
+						return isSuccessful;
+					});
+				});
+		}
+	}
+}

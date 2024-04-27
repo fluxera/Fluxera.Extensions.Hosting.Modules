b7655a456a3e10e0c62c280d9e8da878a5b31c51
Matthias Gernand <mgernand@users.noreply.github.com>
2024-04-27 11:25:30 +0200

    Updated packages. (#158)

---
+++ samples/Components/Ordering/Ordering.HttpApi/Endpoints/Orders/GetOrders.cs
@@ -0,0 +1,27 @@
+﻿namespace Ordering.HttpApi.Endpoints.Orders
+{
+	using System.Threading;
+	using System.Threading.Tasks;
+	using JetBrains.Annotations;
+	using MadEyeMatt.AspNetCore.Endpoints;
+	using Microsoft.AspNetCore.Builder;
+	using Microsoft.AspNetCore.Http;
+	using Microsoft.AspNetCore.Routing;
+
+	[PublicAPI]
+	public sealed class GetOrders : EndpointBase
+	{
+		/// <inheritdoc />
+		public override void Map(IEndpointRouteBuilder endpoints)
+		{
+			endpoints
+				.MapGet(this.Execute)
+				.AllowAnonymous();
+		}
+
+		private Task<IResult> Execute(CancellationToken cancellationToken = default)
+		{
+			return Task.FromResult(Results.Ok());
+		}
+	}
+}
--- samples/Components/Ordering/Ordering.HttpApi/OrderingHttpApiModule.cs
+++ samples/Components/Ordering/Ordering.HttpApi/OrderingHttpApiModule.cs
@@ -4,11 +4,22 @@
 	using Fluxera.Extensions.Hosting.Modules;
 	using Fluxera.Extensions.Hosting.Modules.AspNetCore.HttpApi;
 	using JetBrains.Annotations;
+	using MadEyeMatt.AspNetCore.Endpoints;
+	using Microsoft.Extensions.DependencyInjection;
 
 	[PublicAPI]
 	[DependsOn<HttpApiModule>]
 	public sealed class OrderingHttpApiModule : ConfigureApplicationModule
 	{
+		/// <inheritdoc />
+		public override void ConfigureServices(IServiceConfigurationContext context)
+		{
+			context.Services.Configure<EndpointsOptions>(options =>
+			{
+				options.EndpointsRoutePrefix = "endpoints";
+			});
+		}
+
 		/// <inheritdoc />
 		public override void PreConfigure(IApplicationInitializationContext context)
 		{

9ab7f460cf1f40c29157c7ae3de14be1470d95a9
Matthias Gernand <matthias.gernand@fluxera.com>
2024-05-26 17:48:10 +0200

    Upgraded packages.

--- samples/Components/Catalog/Catalog.HttpClient/Clients/ProductApplicationServiceClient.cs
+++ samples/Components/Catalog/Catalog.HttpClient/Clients/ProductApplicationServiceClient.cs
@@ -1,10 +1,12 @@
 ﻿namespace Catalog.HttpClient.Clients
 {
 	using System.Net.Http;
+	using System.Net.Http.Json;
 	using System.Threading.Tasks;
 	using Catalog.Application.Contracts.Products;
 	using Catalog.Domain.Shared.Products;
 	using Fluxera.Extensions.Hosting.Modules.Application.Contracts.Dtos.Results;
+	using Fluxera.Extensions.Hosting.Modules.HttpClient;
 	using Fluxera.Extensions.Http;
 	using JetBrains.Annotations;
 
@@ -21,7 +23,7 @@
 		public async Task<ResultDto<ProductDto>> GetProductAsync(ProductId id)
 		{
 			HttpResponseMessage response = await this.HttpClient.GetAsync($"/catalog/products/{id}");
-			ResultDto<ProductDto> result = await response.Content.ReadAsAsync<ResultDto<ProductDto>>();
+			ResultDto<ProductDto> result = await response.Content.ReadFromJsonAsync<ResultDto<ProductDto>>(this.GetJsonSerializerOptions());
 
 			return result;
 		}
@@ -31,7 +33,7 @@
 		{
 			HttpContent content = dto.AsJsonContent();
 			HttpResponseMessage response = await this.HttpClient.PostAsync("/catalog/products", content);
-			ResultDto<ProductDto> result = await response.Content.ReadAsAsync<ResultDto<ProductDto>>();
+			ResultDto<ProductDto> result = await response.Content.ReadFromJsonAsync<ResultDto<ProductDto>>(this.GetJsonSerializerOptions());
 
 			return result;
 		}
@@ -40,7 +42,7 @@
 		public async Task<ResultDto<ProductDto[]>> GetProductsAsync()
 		{
 			HttpResponseMessage response = await this.HttpClient.GetAsync("/catalog/products");
-			ResultDto<ProductDto[]> result = (await response.Content.ReadAsAsync<ResultDto<ProductDto[]>>());
+			ResultDto<ProductDto[]> result = await response.Content.ReadFromJsonAsync<ResultDto<ProductDto[]>>(this.GetJsonSerializerOptions());
 
 			return result;
 		}
---
+++ src/Fluxera.Extensions.Hosting.Modules.HttpClient/HttpClientService.cs
@@ -0,0 +1,41 @@
+﻿namespace Fluxera.Extensions.Hosting.Modules.HttpClient
+{
+	using System;
+	using System.Text.Json;
+	using System.Text.Json.Serialization;
+	using Fluxera.Enumeration.SystemTextJson;
+	using Fluxera.StronglyTypedId.SystemTextJson;
+	using Fluxera.ValueObject.SystemTextJson;
+	using JetBrains.Annotations;
+
+	/// <summary>
+	///		Provides the JSON serializer options instance with custom type support.
+	/// </summary>
+	[PublicAPI]
+	public static class HttpClientService
+	{
+		private static readonly Lazy<JsonSerializerOptions> JsonSerializerOptions = new Lazy<JsonSerializerOptions>(() =>
+		{
+			JsonSerializerOptions options = new JsonSerializerOptions
+			{
+				PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+				DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+				Converters =
+				{
+					new JsonStringEnumConverter()
+				}
+			};
+			options.UseEnumeration();
+			options.UsePrimitiveValueObject();
+			options.UseStronglyTypedId();
+
+			return options;
+		});
+
+		/// <summary>
+		///		Gets the custom JSON serializer options.
+		///		The custom options support smart enums, primitive value objects and strongly-typed IDs.
+		/// </summary>
+		public static JsonSerializerOptions Options => JsonSerializerOptions.Value;
+	}
+}

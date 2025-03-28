e6de85d5584534e4743ddfc21ab0357d9559226b
Matthias Gernand <matthias.gernand@fluxera.com>
2025-03-28 21:59:53 +0100

    Stuff

---
+++ src/Fluxera.Extensions.Hosting.Modules.Persistence/EntityCachingBuilder.cs
@@ -0,0 +1,57 @@
+﻿namespace Fluxera.Extensions.Hosting.Modules.Persistence
+{
+	using System;
+	using Fluxera.Repository;
+
+	internal sealed class EntityCachingBuilder : IEntityCachingBuilder
+	{
+		private readonly IEntityCachingOptionsBuilder aggregateCachingOptionsBuilder;
+
+		public EntityCachingBuilder(IEntityCachingOptionsBuilder aggregateCachingOptionsBuilder)
+		{
+			this.aggregateCachingOptionsBuilder = aggregateCachingOptionsBuilder;
+		}
+
+		/// <inheritdoc />
+		public IEntityCachingBuilder UseNoCachingFor<TAggregateRoot>()
+		{
+			this.aggregateCachingOptionsBuilder.UseNoCachingFor<TAggregateRoot>();
+			return this;
+		}
+
+		/// <inheritdoc />
+		public IEntityCachingBuilder UseNoCachingFor(Type type)
+		{
+			this.aggregateCachingOptionsBuilder.UseNoCachingFor(type);
+			return this;
+		}
+
+		/// <inheritdoc />
+		public IEntityCachingBuilder UseStandardFor<TAggregateRoot>()
+		{
+			this.aggregateCachingOptionsBuilder.UseStandardFor<TAggregateRoot>();
+			return this;
+		}
+
+		/// <inheritdoc />
+		public IEntityCachingBuilder UseStandardFor(Type type)
+		{
+			this.aggregateCachingOptionsBuilder.UseStandardFor(type);
+			return this;
+		}
+
+		/// <inheritdoc />
+		public IEntityCachingBuilder UseTimeoutFor<TAggregateRoot>(TimeSpan expiration)
+		{
+			this.aggregateCachingOptionsBuilder.UseTimeoutFor<TAggregateRoot>(expiration);
+			return this;
+		}
+
+		/// <inheritdoc />
+		public IEntityCachingBuilder UseTimeoutFor(Type type, TimeSpan expiration)
+		{
+			this.aggregateCachingOptionsBuilder.UseTimeoutFor(type, expiration);
+			return this;
+		}
+	}
+}
---
+++ src/Fluxera.Extensions.Hosting.Modules.Persistence/IEntityCachingBuilder.cs
@@ -0,0 +1,56 @@
+﻿namespace Fluxera.Extensions.Hosting.Modules.Persistence
+{
+	using System;
+	using JetBrains.Annotations;
+
+	/// <summary>
+	///     A contract for an aggregate-specific caching builder.
+	/// </summary>
+	[PublicAPI]
+	public interface IEntityCachingBuilder
+	{
+		/// <summary>
+		///     Configure not to use caching for the given aggregate root.
+		/// </summary>
+		/// <typeparam name="TAggregateRoot"></typeparam>
+		/// <returns></returns>
+		IEntityCachingBuilder UseNoCachingFor<TAggregateRoot>();
+
+		/// <summary>
+		///     Configure not to use caching for the given aggregate root.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		IEntityCachingBuilder UseNoCachingFor(Type type);
+
+		/// <summary>
+		///     Configure to use the standard caching strategy for the given aggregate root.
+		/// </summary>
+		/// <typeparam name="TAggregateRoot"></typeparam>
+		/// <returns></returns>
+		IEntityCachingBuilder UseStandardFor<TAggregateRoot>();
+
+		/// <summary>
+		///     Configure to use the standard caching strategy for the given aggregate root.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		IEntityCachingBuilder UseStandardFor(Type type);
+
+		/// <summary>
+		///     Configure to use the timeout caching strategy for the given aggregate root.
+		/// </summary>
+		/// <typeparam name="TAggregateRoot"></typeparam>
+		/// <param name="expiration"></param>
+		/// <returns></returns>
+		IEntityCachingBuilder UseTimeoutFor<TAggregateRoot>(TimeSpan expiration);
+
+		/// <summary>
+		///     Configure to use the timeout caching strategy for the given aggregate root.
+		/// </summary>
+		/// <param name="type"></param>
+		/// <param name="expiration"></param>
+		/// <returns></returns>
+		IEntityCachingBuilder UseTimeoutFor(Type type, TimeSpan expiration);
+	}
+}
